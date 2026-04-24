60d10d924e39cabd72cd852b792fa3790a609e5f
ljw2044 <144752394+ljw2044@users.noreply.github.com>
2026-04-24 09:45:28 +0900

    1-4c

---
+++ code/1-4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int my_strcmp(const char *a, const char *b) {
+    while (*a != '\0' && (*a == *b)) {
+        a++;
+        b++;
+    }
+
+    return *(unsigned char *)a - *(unsigned char *)b;
+}
+
+int main() {
+
+    printf("%d\n", my_strcmp("abc", "abcd"));
+    printf("%d\n", my_strcmp("abc", "abc" ));
+    printf("%d\n", my_strcmp("b"  , "a"   ));
+
+    return 0;
+}
